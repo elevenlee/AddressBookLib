9cb95c4051451737e8be42d208b84ae5127449e1
shenli <sl3268@nyu.edu>
2014-11-30 14:52:20 -0800

    Add contact context search criteria interface

---
+++ src/edu/nyu/cs/addressbook/criteria/SearchCriteria.java
@@ -0,0 +1,22 @@
+package edu.nyu.cs.addressbook.criteria;
+
+import edu.nyu.cs.addressbook.ContactEntry;
+
+/**
+ * @author shenli
+ * <p>
+ * A class can implement the {@code SearchCriteria} interface when it wants to impose a searching criteria on 
+ * the objects.
+ */
+public interface SearchCriteria {
+    
+    /**
+     * This method is called whenever checking the object is matched the specified condition. Returns true if 
+     * the element matches the specified condition, otherwise false.
+     * <p>
+     * @param element the element
+     * @return true if the element matches the specified condition, otherwise false
+     */
+    public boolean matchCondition(ContactEntry element);
+    
+}
